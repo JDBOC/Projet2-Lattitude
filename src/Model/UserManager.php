7c7a88f062539c7fe9f4d8900bd95d8f76a7be63
Jean Daniel Boccara <Jean_Daniel_Boccara_student2019@wilder.school>
2019-04-11 13:21:51 +0200

    Usercontroller et UserManager

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -1,19 +1,21 @@
 <?php
+namespace App\Model;
 
 class UserManager extends \App\Model\AbstractManager
 {
-	/*enregistrement dans la BDD*/
-	function addUser($pdo, $lastname, $firstname, $email, $password)
-	{
-		$insert = 'INSERT INTO users (firstname, lastname, email, password) VALUES (:firstname, :lastname, :email, :password)';
-		
-		$prep = $pdo->prepare($insert);
-		
-		$prep->bindValue('firstname', $firstname, PDO::PARAM_STR);
-		$prep->bindValue('lastname', $lastname, PDO::PARAM_STR);
-		$prep->bindValue('email', $email, PDO::PARAM_STR);
-		$prep->bindValue('password', $password, PDO::PARAM_STR);
-		
-		$prep->execute();
-	}
+    /*enregistrement dans la BDD*/
+    public function addUser($lastname, $firstname, $email, $password)
+    {
+        $insert = 'INSERT INTO users (firstname, lastname, email, password)
+        VALUES (:firstname, :lastname, :email, :password)';
+        
+        $prep = $this->pdo->prepare($insert);
+        
+        $prep->bindValue('firstname', $firstname, \PDO::PARAM_STR);
+        $prep->bindValue('lastname', $lastname, \PDO::PARAM_STR);
+        $prep->bindValue('email', $email, \PDO::PARAM_STR);
+        $prep->bindValue('password', $password, \PDO::PARAM_STR);
+        
+        $prep->execute();
+    }
 }
